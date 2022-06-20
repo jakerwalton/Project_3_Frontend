d9d818b45840fc39bd731023df38c67e62310ccf
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-20 10:59:57 -0500

    css on cards

--- src/components/Restaurant-card.js
+++ src/components/Restaurant-card.js
@@ -18,9 +18,9 @@ function Card(props) {
         <h3>{restaurant.cuisine[0]}</h3>
         <h4>{restaurant.userRating} / 5</h4>
         <p>Reviewed by: {restaurant.user}</p>
-        <p>{restaurant.comments}</p>
+        {/* <p>{restaurant.comments}</p> */}
         <li>
-                <a href={restaurant.website}  className="active">
+                <a href={restaurant.website} id="weblink">
                     {restaurant.name}
                 </a>
             </li>
