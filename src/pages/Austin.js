013e71b7185785b0c11942478a6e11ad0b06b254
Dave <dlp140@gmail.com>
2022-06-20 12:44:34 -0400

    revert to previous version

--- src/pages/Austin.js
+++ src/pages/Austin.js
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+import Card from "../components/Restaurant-card";
+
 function Austin(props) {
-    return (
-        <h1>The Austin Page</h1>
-    );
-};
+  const [restaurants, setRestaurants] = useState(null);
+
+  const URL = "https://project-3-backend-hard.herokuapp.com/austin";
+
+  const getAustinRestaurants = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setRestaurants(data);
+  };
+
+  useEffect(() => {
+    getAustinRestaurants();
+  }, []);
+
+  return (
+    <div>
+      <h1>The Austin Page</h1>
+      <div className="card-container">
+        <Card restaurants={restaurants} />
+      </div>
+    </div>
+  );
+}
 
 export default Austin;
