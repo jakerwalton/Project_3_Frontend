9bc9d78d076448b2c4129af259f8cf23d8f22a88
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-06-21 12:21:30 -0400

    added stying+grid to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Home(props) {
   return (
-    <>
+    <div className="container">
       <div id="week"></div>
       <div className="houston">houston</div>
       <div className="detroit">detroit</div>
       <div className="austin">austin</div>
       <div className="raleigh">raleigh</div>
       <h1>Home</h1>
-    </>
+    </div>
   )
 }
 
