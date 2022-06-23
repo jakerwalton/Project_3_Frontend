1785cdc1d4832d4d25c137ca7500da4136368ba9
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-06-23 09:50:11 -0400

    made more links to city pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
-import React from "react"
-import hard from "../assets/hard.png"
-import Dcity from "../assets/Detroit.png"
-import Htown from "../assets/Houstoncity.png"
-import Ralcity from "../assets/Raleigh.png"
-import Austcity from "../assets/austin.png"
-import { Link } from "react-router-dom"
+import React from 'react'
+import hard from '../assets/hard.png'
+import Dcity from '../assets/Detroit.png'
+import Htown from '../assets/Houstoncity.png'
+import Ralcity from '../assets/Raleigh.png'
+import Austcity from '../assets/austin.png'
+import { Link } from 'react-router-dom'
 function Home(props) {
   return (
     <div>
@@ -22,17 +22,23 @@ function Home(props) {
             <button className="new">Add New Restraunt</button>
           </Link>
         </div>
-        <div className="detroit">
-          detroit <img src={Dcity} alt="Detroit" width="200px" height={150} />
-        </div>
-        <div className="austin">
-          austin
-          <img src={Austcity} alt="Austin" width="210px" height={150} />
-        </div>
-        <div className="raleigh">
-          raleigh
-          <img src={Ralcity} alt="Raleigh" width="200px" height={150} />
-        </div>
+        <Link className="detroit" to="/detroit">
+          <div>
+            detroit <img src={Dcity} alt="Detroit" width="200px" height={150} />
+          </div>
+        </Link>
+        <Link className="austin" to="/austin">
+          <div>
+            austin
+            <img src={Austcity} alt="Austin" width="210px" height={150} />
+          </div>
+        </Link>
+        <Link className="raleigh" to="/raleigh">
+          <div>
+            raleigh
+            <img src={Ralcity} alt="Raleigh" width="200px" height={150} />
+          </div>
+        </Link>
       </div>
     </div>
   )
